9d9882aed8b1e5ca01e1af38915e2d81729dc50f
Andrew Beyer <beyer@1password.com>
2021-01-20 04:40:54 -0700

    Initial Commit

---
+++ safari-web-extension-sample/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  safari-web-extension-sample
+//
+//  Created by Andrew Beyer on 1/20/21.
+//
+
+import Cocoa
+import SafariServices.SFSafariApplication
+import SafariServices.SFSafariExtensionManager
+
+let appName = "safari-web-extension-sample"
+let extensionBundleIdentifier = "dev.beyer.safari-web-extension-sample.Extension"
+
+class ViewController: NSViewController {
+
+    @IBOutlet var appNameLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.appNameLabel.stringValue = appName
+        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: extensionBundleIdentifier) { (state, error) in
+            guard let state = state, error == nil else {
+                // Insert code to inform the user that something went wrong.
+                return
+            }
+
+            DispatchQueue.main.async {
+                if (state.isEnabled) {
+                    self.appNameLabel.stringValue = "\(appName)'s extension is currently on."
+                } else {
+                    self.appNameLabel.stringValue = "\(appName)'s extension is currently off. You can turn it on in Safari Extensions preferences."
+                }
+            }
+        }
+    }
+    
+    @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
+            guard error == nil else {
+                // Insert code to inform the user that something went wrong.
+                return
+            }
+
+            DispatchQueue.main.async {
+                NSApplication.shared.terminate(nil)
+            }
+        }
+    }
+
+}
